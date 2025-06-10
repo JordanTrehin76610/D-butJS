c50c48709e7db0313418a55692f20bbe01eea585
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-10 14:17:38 +0200

    Ptite maj de life quality

--- exo5/assets/js/script.js
+++ exo5/assets/js/script.js
@@ -1,3 +1,4 @@
 let nombre1 = +prompt("Rentrez un premier nombre")
 let nombre2 = +prompt("Rentrez un second nombre")
-alert(nombre1 + nombre2)
+let resultat = nombre1 + nombre2
+alert("Le resultat de " + nombre1 + " et " + nombre2 + " est " + resultat)
