cac25460fc1ee86c4b3a29f8e8704b98eba3d7b4
JordanTrehin76610 <jordantrehinpro@gmail.com>
2025-06-10 15:23:13 +0200

    Exo 6

---
+++ exo6/assets/js/script.js
@@ -0,0 +1,17 @@
+let james = "Je m'appelle BOND"
+let faux = true
+let nombreSacré = 85
+let pi = 3.14
+let date = new Date()
+
+console.log(`Type de james:  ${typeof james}`)
+console.log(`Type de faux:  ${typeof faux}`)
+console.log(`Type de nombreSacré:  ${typeof nombreSacré}`)
+console.log(`Type de pi:  ${typeof pi}`)
+console.log(`Type de date:  ${typeof date}`)
+
+const President = "President"
+
+console.log(President)
+
+// const President = "Sénateur"
